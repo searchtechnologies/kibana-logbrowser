27e2e253dd12406cc6b833a44e430fd8335e4e7d
ealvarado <Fall112358>
2017-03-07 15:55:38 -0600

    change version and name

--- index.js
+++ index.js
@@ -8,20 +8,7 @@ export default function (kibana) {
                 title: 'Log Browser',
                 description: 'Log Browser for Big Data',
                 icon: 'plugins/log_browser/icon.png',
-                main: 'plugins/log_browser/app',
-               /*autoload: [].concat(
-                    kibana.autoload.styles,
-                    'ui/chrome',
-                    'ui/state_management/app_state',
-                    'ui/state_management/global_state',
-                    'ui/notify/notifier',
-                    'ui/timefilter',
-                    'ui/routes',
-                    'ui/modules',
-                    'ui/state_management/state',
-                    'angular',
-                    'ui-bootstrap'
-                )*/
+                main: 'plugins/log_browser/app'
             }
         },
         init(server, options) {
